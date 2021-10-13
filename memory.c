bff3a7ff6405024e7434c84f5eb695782ecd5bcd
Jeremiah John <jeremiah.john@utdallas.edu>
2021-10-13 19:39:41 -0400

    did not include all modified files in last commit. loadProgram successfully implemented

--- memory.c
+++ memory.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
-
+#include <errno.h>
+#include "memory.h"
 
 unsigned char mainMemory[4096];
+extern FILE *currentGame;
+int loadProgram(char *gameFileName)
+{
+	currentGame = fopen(gameFileName,"r");
+	if(currentGame == NULL) //if there was a problem opening the file
+	{
+		perror("Error Opening Game File: ");
+	}
+	else
+	{
+		unsigned char *nextBlock = mainMemory; //contains the next block of data in the mainmemory array to write to
+		while (!feof(currentGame))
+		{
+			fread(nextBlock, sizeof(unsigned char),1,currentGame);
+			nextBlock++;
+		}
+	}
+	return 0;
+}
