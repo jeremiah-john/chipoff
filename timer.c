1052a8c5e4a62a51e893692dd2bfd5e5a00f6112
Jeremiah John <jeremiahjohn08706@gmail.com>
2022-01-15 18:07:39 -0600

    going to use stdint.h, need to figure out best way to structure the timer.h file (include stdint.h in there only or also in timer.c)
    Signed-off-by: Jeremiah John <jeremiahjohn08706@gmail.com>

--- timer.c
+++ timer.c
@@ -1,6 +1,12 @@
 #include <time.h>
+#include <stdint.h>
 #include "timer.h"
-
+//I think I set the timers as unsigned chars because of the space but 
+//this might mess with the way the program interprets the numbers
+//casting an unsigned char to an integer might be hard
+//we need a way to keep these variables at the required bitsize for the registers
+//and also keep them as readable numbers
+//we'll use stdint.h
 unsigned char delayTimer = 0;
 unsigned char soundTimer = 0;
 
@@ -21,4 +27,10 @@ unsigned char setSoundTimerVal(unsigned char *sourceRegister)
 	soundTimer = *sourceRegister;
 	return 0;
 }
-
+void decrementDT()
+{
+	if (delayTimer != 0)
+	{
+		delayTimer--;
+	} //this is a placeholder, we'll figure it out later
+}
--- timer.h
+++ timer.h
@@ -2,5 +2,7 @@
 #define TIMER_HEADER
 unsigned char getDelayTimerVal(unsigned char *destRegister);
 unsigned char setDelayTimerVal(unsigned char *sourceRegister);
+void decrementDT();
+void decrementST();
 unsigned char setSoundTimerVal(unsigned char *sourceRegister);
 #endif
